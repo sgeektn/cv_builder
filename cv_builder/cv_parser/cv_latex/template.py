02e49a337ae4a76e42ceea90ac1ee863b2bface6
fakhfakh sami <s-man@MacBook-Pro-de-fakhfakh-2.local>
2019-08-25 22:18:01 +0200

    adding latex

--- cv_builder/cv_parser/cv_latex/template.py
+++ cv_builder/cv_parser/cv_latex/template.py
@@ -8,12 +8,17 @@ def create_template(config):
 		template.write("\\profilepic{about-img.jpg} % Profile picture\n")
 		template.write("\\cvname{"+config["general"]["name"].upper()+"} % Your name\n")
 		template.write("\\cvjobtitle{\hspace{1.2cm}"+config["general"]["title"]+"}  % Job title/career\n")
-		template.write("\\cvdate{09 Novembre 1995 a Lyon} % Date of birth\n") #TODO
-		template.write("\\cvaddress{2 Rue Saint Exupery\n")#TODO
-		template.write("\\newline\n")
-		template.write("92360 ,Meudon la forêt\n")
-		template.write("\\newline\n")
-		template.write("France} \n")
+		template.write("\\cvdate{"+config["general"]["date_of_birth"]+" "+config["general"]["place_of_birth"]+"} % Date of birth\n") 
+		template.write("\\cvaddress{")
+		i=0
+		for adress_words in config["contact"]["adress"].split(" "):
+			template.write(adress_words+" ")
+			if i!=0 and (i+1)%4 == 0:
+				template.write("\n\\newline\n")
+			i+=1
+
+		
+		template.write("} \n")
 		template.write("\\cvnumberphone{"+config["general"]["phone"]+"} % Phone number\n")
 		template.write("\\cvmail{"+config["contact"]["mail"]+"} \n")
 		template.write("\\cvsite{"+config["general"]["website"]+"}\n")
@@ -30,13 +35,30 @@ def create_template(config):
 			i+=1
 		template.write("}\n")
 
-		#todo langages
+		template.write("\\skillss{\n")
+		i=0
+		for skill in config["general"]["languages"]:
+			template.write("	{"+skill["language"]+" "+skill["level"]+"/"+str(float(skill["rate"][:-1])*6/100)+"}")
+			if i < len(config["skills"]["list"])-1:
+				template.write(",\n")
+			i+=1
+		template.write("}\n")
+
 
 		template.write("\\makeprofile \n")
-		template.write("\\section{CURRICULUM VITAE} \n")
+		template.write("\\section{CURRICULUM VITAE} \n\n")
 		
+		for section in config["sections"]:
+			template.write("\\\\\n\\section{"+section["caption"]+"}\n")
+			template.write("\n")
+			template.write("\\begin{twentyshort}\n")
+			for item in section["list"]:
+				template.write("\\twentyitemshort{"+item["date"]+"}{["+item["title"]+"]"+item["label"]+"\n\\newline\n "+item["description"]+" }\n")
+				template.write("\\\\\n")
+			template.write("\\end{twentyshort}\n")
 
-
+		
+		template.write("\\end{document}\n")
 if __name__ == '__main__':
 	with open("../fr.json","r") as file_config:
 		create_template(json.loads(file_config.read()))
